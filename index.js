e3260de8afc5901a77676b8bdf066c35b87f268e
Sarath VS <sarathvss65@gmail.com>
2021-08-17 18:01:39 +0530

    Add projects

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 var app = new Vue({
   el: "#app",
   data: {
-    name: "NETOORAN"
-  }
+    firstName: "Sarath",
+    lastName: "Sasindrakumar",
+    profession: "Software Consultant",
+    company: "ThoughtWorks",
+    projects: [
+      {
+        name: "Live Wall",
+        description: "A website that plays live wallpapers from veriety of categories.",
+        url: "https://netooran.github.io/live-wall/",
+        github: "https://github.com/netooran/live-wall",
+        stack: ["html", "css", "js"],
+      },
+      {
+        name: "Blog",
+        description: "I write various aspects of frontend development",
+        url: "https://netooran.github.io/blog/",
+        github: "https://github.com/netooran/blog",
+        stack: ["html", "css", "js"],
+      },
+    ],
+  },
 });
